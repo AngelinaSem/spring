0e3c935687c70396a6e42735b32ba77709d20c1c
AngelinaSem <71393499+AngelinaSem@users.noreply.github.com>
2022-05-11 18:22:55 +0300

    Delete src/main/java/com/example/spring directory

--- src/main/java/com/example/spring/rep/Shagjparep.java
+++
@@ -1,9 +0,0 @@
-package com.example.spring.rep;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.example.spring.osnova.Shag;
-
-@Repository
-public interface Shagjparep extends JpaRepository<Shag, Long> {
-}
